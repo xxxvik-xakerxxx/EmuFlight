d177c2168582690aeb7630947a9315882cc2f825
nerdCopter <56646290+nerdCopter@users.noreply.github.com>
2023-06-04 11:00:25 -0500

    FOXEERF722V4 - MPU6000, ICM42688P
    remove mpu6500 driver

--- src/main/target/FOXEERF722V4/target.mk
+++ src/main/target/FOXEERF722V4/target.mk
@@ -8,6 +8,5 @@ TARGET_SRC = \
             drivers/max7456.c \
             drivers/accgyro/accgyro_mpu6500.c \
             drivers/accgyro/accgyro_spi_mpu6000.c \
-            drivers/accgyro/accgyro_spi_mpu6500.c \
             drivers/accgyro/accgyro_mpu.c \
             drivers/accgyro/accgyro_spi_icm426xx.c
